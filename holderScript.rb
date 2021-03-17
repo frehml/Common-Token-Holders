44e51cc2a7df623c39263abc51bc2be4c829a6c4
frehml <frederic.hamelink@hotmail.com>
2021-03-17 16:49:24 +0100

    show balances and dust coin threshold

--- holderScript.rb
+++ holderScript.rb
@@ -1,37 +1,47 @@
 require 'csv'
 
+#this class parses the common holders of a token out of a csv file and prints them
 class CommonHolders
     #Initializes class CommonHolders with an array of filenames
-    def initialize(filenames)
+    def initialize(filenames, threshold)
         @filenames = filenames
-        @commonHolders = []
+        @threshold = threshold
+        @h
         parse()
     end
 
-    #parses the data from the csv files
+    #parses the data from the csv files using the csv library
     def parse()
         files = []
         @filenames.each do |filename|
             files.push(CSV.parse(File.read(filename), headers: true))
         end
-        getCommons(files)
+        turnHash(files)
     end
 
-    #gets common holders
-    def getCommons(files)
-        @commonHolders = files[0].by_col[0]
-        files[1..-1].each do |file|
-            @commonHolders = @commonHolders & file.by_col[0]
+    #merges the tables into a hash (address => [balances])
+    def turnHash(files)
+        @h = Hash.new{|hash, key| hash[key] = Array.new()}
+        files.each.with_index do |file, idx|
+            file.each do |row|
+                #makes balances only get added if its a common holder
+                #this is more memory efficient
+                @h[row[0]] << row[1] if @h[row[0]].length() == idx && ("%f" % row[1]).to_f > @threshold
+                #if there are balances for all tickers then print
+                p(row[0]) if @h[row[0]].length() == @filenames.length()
+            end
         end
     end
 
-    #prints out common holders
-    def print()
-        @commonHolders.each do |holder|
-            puts(holder)
+    #prints the address and balance for individual addresses
+    #"Address ticker: balance"
+    def p(address)
+        puts(address)
+        @filenames.each.with_index do |filename, idx|
+            puts(filename[0...-4] + ": " + @h[address][idx].to_s)
         end
+        puts("\n")
     end
 end
 
-holders = CommonHolders.new(["fxs.csv", "ersdl.csv"])
-holders.print()
+holders = CommonHolders.new(["fxs.csv", "ersdl.csv"], 1) #([tickers], float as dust coin threshold)
